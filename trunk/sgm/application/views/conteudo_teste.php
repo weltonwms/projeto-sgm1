e7bf3b97fd2ee2b6b37b83869e6350f58db840d9
weltonwms@gmail.com <weltonwms@gmail.com@74f0f9e3-fe07-e33f-c878-47fb525c43d1>
2014-04-15 02:13:08 +0000

    submit do cicle

--- trunk/sgm/application/views/conteudo_teste.php
+++ trunk/sgm/application/views/conteudo_teste.php
@@ -1,1 +1,18 @@
-<section id="conteudo">conteudo</section>
+
+
+<script src="<?php echo base_url('assets/plugins/jquery.cycle.all.js')?>"></script>
+ 
+ 
+<script type="text/javascript" charset="utf-8">
+$(document).ready(function(){
+  $('#slide').cycle( {
+    sync:  false,
+    fx: 'zoom'
+  } );
+});
+</script>
+
+<div id="slide">
+     <img src="<?php echo base_url('assets/imgs/logo1.png')?>"  width="580" height="200" />
+        <img src="<?php echo base_url('assets/imgs/logo1.png')?>" width="580" height="200" />
+</div>
